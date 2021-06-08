68ce5b5371807e2289e447031e3e5e6cc65d1a21
DiosAkhilleus <abertin1809@gmail.com>
2021-06-08 16:44:05 -0500

    created basic page layout and card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [names, setNames] = useState([
+    'Timmy',
+    'Chimbles',
+    'Fred',
+    'Griswold',
+    'Stew',
+    'Mud', 
+    'Jessica',
+    'Pitts', 
+    'Andy',
+    'Snoopy',
+    'Bobo',
+    'Skippy'
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='header'>Memory Card Game</header>
+      <div className='content-container'>
+        {names.map((el, ind) => <Card name={names[ind]}/>)}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = ({ name }) => {
+    return (
+        <div className='card'>
+            {name}
+        </div>
+    )
+}
+
+export default Card
